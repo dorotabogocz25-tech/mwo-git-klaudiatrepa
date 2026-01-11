1a615d794568e2fd7c7529d1c0e2ce2b6050f038
Dorota Bogocz <doris@MacBook-Air-Dorota.local>
2026-01-11 11:57:07 +0100

    added rocket method

--- SomeProgram.java
+++ SomeProgram.java
@@ -1,6 +1,12 @@
 public class SomeProgram {
-    public static void main(String[] args) {
-        System.out.println("We are learning to use Git.");
-        System.out.println("We will be Git experts, soon.");
-    }
+    public void manageRockets() {
+        System.out.println("3,2,1");
+    // TODO implement
 }
+public static void main(String[] args) {
+    System.out.println("We are learning to use Git.");
+    System.out.println("We will be Git experts, soon.");
+}
+        
+    }
+
